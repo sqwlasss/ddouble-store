fa4974f3951579e90b784c0d58b4f0208f6e4a57
sqwlasss <sqwlasss@users.noreply.github.com>
2026-07-30 18:17:47 +0300

    Replace office lifestyle image with local photo_vertical.jpg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,12 +7,13 @@ import Footer from "@/components/ddouble/Footer";
 import { useCurrency } from "@/lib/CurrencyContext";
 import { useCollections, useCollectionProducts } from "@/hooks/useProducts";
 import homeHero from "../../assets/home.jpg";
+import photoVertical from "../../assets/photo_vertical.jpg";
 
 const LIFESTYLE_IMAGES = {
   bedroom: "https://media.base44.com/images/public/6a590df7244fc537b99549d8/7efe5efef_generated_d41c0030.png",
   dining: "https://media.base44.com/images/public/6a590df7244fc537b99549d8/e26fb1318_generated_cabe50da.png",
   galleryWall: "https://media.base44.com/images/public/6a590df7244fc537b99549d8/8140dfca5_generated_d1feca24.png",
-  office: "https://media.base44.com/images/public/6a590df7244fc537b99549d8/05a5cfb7a_generated_75024acd.png",
+  office: photoVertical,
 };
 
 const TESTIMONIALS = [
